c68505d44c5c3efd9b2039e72cb4434fcd28f33d
Тугарин Руслан Евгеньевич <ip015s30@bwp07.csc.local>
2021-03-17 16:30:18 +0700

    fixed regex

--- geometry.cpp
+++ geometry.cpp
@@ -1,9 +1,30 @@
+#include <cstring>
 #include <iostream>
+#include <regex>
+
 using namespace std;
+
+void circle(string CIRCLE)
+{	 
+	if ((strstr(CIRCLE.c_str(), "circle"))
+		|| (strstr(CIRCLE.c_str(),"circle("))){
+	cmatch result;
+	regex regular(
+		"([\\w\\s]+)(\\()([\\s]*)([0-9]+)([\\s]*)([0-9]+)([\\s]*)(\\,)([\\s]*)([0-9]*[.]?[0-9]+)([\\s]*)(\\))"
+);
+
+        if (regex_match(CIRCLE.c_str(), result, regular)) {
+            cout << "true" << endl;        
+}    else
+            cout << "error" << endl;
+    } else
+        cout << "error" << endl;
+}
+
 int main()
 {
-	char str[256];
-	cin.getline(str,256,';');
-	cout<<str<<endl;
-	return 0;
+    string CIRCLE; 
+    getline(cin, CIRCLE);
+    circle(CIRCLE);
+    return 0;
 }
