3091d86aa42586e20089ba4a40f1699e38e22e52
Тугарин Руслан Евгеньевич <ip015s30@bwp07.csc.local>
2021-04-28 16:18:27 +0700

    fixed geometry.cpp

--- geometry.cpp
+++ geometry.cpp
@@ -4,13 +4,24 @@
 
 using namespace std;
 
-void circle(string CIRCLE)
+void proverka(string CIRCLE)
 {	 
-	if ((strstr(CIRCLE.c_str(), "circle"))
+	if ((strstr(CIRCLE.c_str(), "circle "))
 		|| (strstr(CIRCLE.c_str(),"circle("))){
 	cmatch result;
 	regex regular(
-		"([\\w\\s]+)(\\()([\\s]*)([0-9]+)([\\s]*)([0-9]+)([\\s]*)(\\,)([\\s]*)([0-9]*[.]?[0-9]+)([\\s]*)(\\))"
+		"([\\w\\s]+)"
+		"(\\()"
+		"([\\s]*)"
+		"([0-9]*[.]?[0-9]+)"
+		"([\\s]*)"
+		"([0-9]*[.]?[0-9]+)"
+		"([\\s]*)"
+		"(\\,)"
+		"([\\s]*)"
+		"([0-9]*[.]?[0-9]+)"
+		"([\\s]*)"
+		"(\\))"
 );
 
         if (regex_match(CIRCLE.c_str(), result, regular)) {
@@ -23,8 +34,9 @@ void circle(string CIRCLE)
 
 int main()
 {
+    cout<< "Enter a shape (example: circle (2 2, 2.2) ): \n" ;
     string CIRCLE; 
     getline(cin, CIRCLE);
-    circle(CIRCLE);
+    proverka(CIRCLE);
     return 0;
 }
